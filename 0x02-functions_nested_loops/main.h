9bb007df17be4bdd5db9481487cc79c50b5a0325
Charleskojomark <markcharleskojo@gmail.com>
2023-02-23 21:51:51 -0800

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,7 @@
-#include "main.h"
-/**
- * main - Entry point
- *
- * Description - print _putchar
- *
- * Return: always 0
- */
-int main(void)
-{
-	char c[] = "_putchar";
-	int i;
+#ifndef main_h
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
-}
+#define main_h
 
+extern int _putchar(char c);
+
+#endif
